956dfbd658229188cc365475b75ee08551a6293c
goldberg0073 <hicham_goldberg@hotmail.com>
2019-12-03 00:40:32 -0500

    TheRest

--- src/test/java/com/cybertek/tests/practice/TestBase.java
+++ src/test/java/com/cybertek/tests/practice/TestBase.java
@@ -1,4 +1,32 @@
 package com.cybertek.tests.practice;
 
+import com.cybertek.utilities.ConfigurationReader;
+import com.cybertek.utilities.Driver;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
 public class TestBase {
+
+    protected WebDriver driver;
+
+    protected  String url;
+
+    @BeforeMethod
+    public void setupMethod() {
+        driver = Driver.get();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        url =  ConfigurationReader.get("url");
+        driver.get(url);
+
+    }
+
+    @AfterMethod
+    public void teardown() throws InterruptedException {
+        Thread.sleep(1000);
+        Driver.closeDriver();
+    }
+
 }
